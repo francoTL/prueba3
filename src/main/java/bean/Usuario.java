e4916c4c240dba22791a410e67dc5e8d6379e30e
unknown <Franco@DCS-59.apstalca.intranet>
2024-01-08 11:52:33 -0400

    first commit

---
+++ src/main/java/bean/Usuario.java
@@ -0,0 +1,36 @@
+package bean;
+
+public class Usuario {
+
+	private String usuario;
+	private String clave;
+
+	public Usuario(String usuario, String clave) {
+
+		this.usuario = usuario;
+		this.clave = clave;
+	}
+
+	public Usuario() {
+
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getClave() {
+		return clave;
+	}
+
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+
+	
+
+}
---
+++ src/main/java/utils/MySQLConexion.java
@@ -0,0 +1,26 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLConexion {
+	public static Connection getConexion() {
+		Connection con = null;
+		try {
+			
+			Class.forName("com.mysql.jdbc.Driver");
+			String url = "jdbc:mysql://localhost/usuarios";
+			String usr = "root";
+			String psw = "";
+			
+			con = DriverManager.getConnection(url, usr, psw);
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("Error >> Driver no Instalado!!");
+		} catch (SQLException e) {
+			System.out.println("Error >> de conexi�n con la BD");
+		}
+		return con;
+	}
+}
